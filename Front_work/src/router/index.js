c400bbef61c61f2a3c0d5d037f6c4d3a6104b4af
sangmin song <ssmin940606@gmail.com>
2021-05-22 00:59:00 +0900

    solve error

--- Front_work/src/router/index.js
+++ Front_work/src/router/index.js
@@ -1,21 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import MainPage from '@/views/MainPage.vue'
-import practice from '@/views/practice.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'MainPage',
-    component: MainPage
-  },
-  {
-    path: '/practice',
-    name: 'practice',
-    component: practice
-  },
+
 ]
 
 const router = new VueRouter({
