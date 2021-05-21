00c8ae64698091ca57497898affd8e526cc9e4d2
sangmin song <ssmin940606@gmail.com>
2021-05-22 00:43:33 +0900

    restruct folder structure

---
+++ Front_work/src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- 로그인 창 구현 -->
+    <!-- 표현 방식 고민 필요 -->
+  </div>
+</template>
+
+<script>
+export default {
+  // 로직 구현
+}
+
+
+</script>
+
+<style>
+
+</style>
---
+++ Front_work/src/views/Profile.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- 유저 이름, 닉네임, 사진 -->
+    <UserInfo/>
+    <!-- 유저 정보 변경을 위한 modal-->
+    <!-- v-for로 반복 돌리기 -->
+    <PhotoTicket/>
+    <!-- inifite scroll 구현 -->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
